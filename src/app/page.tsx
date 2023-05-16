71c031c47ab4afd3ef86147076b5073f79704e14
Phil Leggetter <phil@leggetter.co.uk>
2023-05-16 14:05:37 +0100

    chore: add link around title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,9 @@ export default async function Home({
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full items-center justify-between text-xl font-bold lg:flex">
-        <div className="text-3xl">🎥 Tigris Movie Database</div>
+        <div className="text-3xl">
+          <a href="/">🎥 Tigris Movie Database</a>
+        </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
