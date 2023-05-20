6466d8de48d58e7c3695728b7fb3d6291de1f79e
Phil Leggetter <phil@leggetter.co.uk>
2023-05-20 12:29:56 +0100

    chore: filter more/less icons

---
+++ src/icons/chevron-up.tsx
@@ -0,0 +1,18 @@
+export const ChevronUp = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 15.75l7.5-7.5 7.5 7.5"
+      />
+    </svg>
+  );
+};
